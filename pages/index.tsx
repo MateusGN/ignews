fd55673c1bfe05f733cb235b7147b0d234d23d50
MateusKha <mateusgnascimento@outlook.com>
2022-02-01 00:44:25 -0300

    GlobalStyles and document

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
-import styles from "../styles/home.module.scss";
+import Head from "next/head";
 
 export default function Home() {
-  return <div className={styles.title}>Hellow World</div>;
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <div>Hellow World</div>
+    </>
+  );
 }
